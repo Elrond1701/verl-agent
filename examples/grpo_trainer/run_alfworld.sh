61dcd82de7471253e3f4861436522d62fe5a714a
zy7chen <zy7chen@umd.edu>
2026-02-23 17:38:36 -0500

    fix(alfworld): alfworld setting

--- examples/grpo_trainer/run_alfworld.sh
+++ examples/grpo_trainer/run_alfworld.sh
@@ -2,7 +2,7 @@ set -x
 ENGINE=${1:-vllm}
 export VLLM_ATTENTION_BACKEND=XFORMERS
 
-num_cpus_per_env_worker=1 # The CPU resource allocated for each environment worker. If you want to use less CPU resources, you can decrease this value.
+num_cpus_per_env_worker=0.1 # The CPU resource allocated for each environment worker. If you want to use less CPU resources, you can decrease this value.
 
 train_data_size=8
 val_data_size=128
