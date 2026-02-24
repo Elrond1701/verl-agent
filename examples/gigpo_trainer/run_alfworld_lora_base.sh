193a46ed00d17f906f3e267463fc3b1c2c732f43
zy7chen <zy7chen@umd.edu>
2026-02-24 16:03:56 -0500

    fix(alfworld): flash attention

--- examples/gigpo_trainer/run_alfworld_lora_base.sh
+++ examples/gigpo_trainer/run_alfworld_lora_base.sh
@@ -39,6 +39,7 @@ python3 -m verl.trainer.main_ppo \
     actor_rollout_ref.model.enable_gradient_checkpointing=True \
     actor_rollout_ref.actor.fsdp_config.param_offload=False \
     actor_rollout_ref.actor.fsdp_config.optimizer_offload=False \
+    actor_rollout_ref.actor.fsdp_config.model_dtype=bf16 \
     actor_rollout_ref.rollout.log_prob_micro_batch_size_per_gpu=32 \
     actor_rollout_ref.rollout.tensor_model_parallel_size=2 \
     actor_rollout_ref.rollout.name=$ENGINE \
