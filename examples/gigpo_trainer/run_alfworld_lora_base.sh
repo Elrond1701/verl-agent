14032a4e8fb427dcdc8bbcecaca10720a1c182a8
zy7chen <zy7chen@umd.edu>
2026-02-25 02:55:25 -0500

    1

--- examples/gigpo_trainer/run_alfworld_lora_base.sh
+++ examples/gigpo_trainer/run_alfworld_lora_base.sh
@@ -1,11 +1,12 @@
 set -x
+ulimit -u 65536 | true
 ENGINE=${1:-vllm}
 export VLLM_ATTENTION_BACKEND=FLASH_ATTN
 
 num_cpus_per_env_worker=0.1 # The CPU resource allocated for each environment worker. If you want to use less CPU resources, you can decrease this value.
 
 train_data_size=16
-val_data_size=128
+val_data_size=92
 group_size=8
 mode="mean_std_norm" # "mean_norm" or "mean_std_norm"
 export WANDB_ENTITY="${WANDB_ENTITY:-Learn2Explore}"
