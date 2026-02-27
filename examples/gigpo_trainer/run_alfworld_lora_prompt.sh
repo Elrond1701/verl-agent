d1365cef97a4757fc8d539b1472ce4c1cc2d3133
zy7chen <zy7chen@umd.edu>
2026-02-27 00:22:32 -0500

    lr=2e-5

--- examples/gigpo_trainer/run_alfworld_lora_prompt.sh
+++ examples/gigpo_trainer/run_alfworld_lora_prompt.sh
@@ -31,7 +31,7 @@ python3 -m verl.trainer.main_ppo \
     actor_rollout_ref.model.path=Qwen/Qwen2.5-1.5B-Instruct \
     actor_rollout_ref.model.lora_rank=32 \
     actor_rollout_ref.model.lora_alpha=32 \
-    actor_rollout_ref.actor.optim.lr=1e-5 \
+    actor_rollout_ref.actor.optim.lr=2e-5 \
     actor_rollout_ref.model.use_remove_padding=True \
     actor_rollout_ref.actor.ppo_mini_batch_size=256 \
     actor_rollout_ref.actor.ppo_micro_batch_size_per_gpu=32 \
@@ -68,7 +68,7 @@ python3 -m verl.trainer.main_ppo \
     trainer.critic_warmup=0 \
     trainer.logger=['console','wandb'] \
     trainer.project_name='verl_agent_alfworld' \
-    trainer.experiment_name='gigpo_qwen2.5_1.5b_no_kl_lora_rank32_alpha32_lr_1e_5_our_prompt' \
+    trainer.experiment_name='gigpo_qwen2.5_1.5b_no_kl_lora_rank32_alpha32_lr_2e_5_our_prompt' \
     trainer.n_gpus_per_node=2 \
     trainer.nnodes=1 \
     trainer.save_freq=-1 \
