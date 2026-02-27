9173d2f557805838f09e171dd70d4493676fab5e
zy7chen <zy7chen@umd.edu>
2026-02-27 15:40:45 -0500

    ppo_micro_batch_size_per_gpu=8

--- examples/gigpo_trainer/run_alfworld_lora_prompt.sh
+++ examples/gigpo_trainer/run_alfworld_lora_prompt.sh
@@ -34,7 +34,7 @@ python3 -m verl.trainer.main_ppo \
     actor_rollout_ref.actor.optim.lr=1e-5 \
     actor_rollout_ref.model.use_remove_padding=True \
     actor_rollout_ref.actor.ppo_mini_batch_size=256 \
-    actor_rollout_ref.actor.ppo_micro_batch_size_per_gpu=32 \
+    actor_rollout_ref.actor.ppo_micro_batch_size_per_gpu=8 \
     actor_rollout_ref.actor.use_kl_loss=False \
     actor_rollout_ref.actor.kl_loss_type=low_var_kl \
     actor_rollout_ref.model.enable_gradient_checkpointing=True \
